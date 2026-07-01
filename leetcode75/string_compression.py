ca332cf4206c4ec9aad3749e1464aa5618f00d26
mokshayagna <mokshayagna09@gmail.com>
2026-07-01 11:34:05 +0530

    adding string compression

--- leetcode75/string_compression.py
+++ leetcode75/string_compression.py
@@ -1,20 +1,18 @@
 chars = ["a","a","b","b","c","c","c"]
 
-q = []
+res = []
 i = 0
-
 while i < len(chars):
     count = 1
-    
-    while i + 1 < len(chars) and chars[i] == chars[i+1]:
+    while i + 1 < len(chars) and chars[i] == chars[i + 1]:
         count += 1
         i += 1
-    
-    q.append(chars[i])
-    
+    res.append(chars[i])
     if count > 1:
-        q.append(str(count))
-    
+        if count > 10:
+            for digit in str(count):
+                res.append(digit)
+        else:
+            res.append(str(count))
     i += 1
-
-print(q)
+print(res)
